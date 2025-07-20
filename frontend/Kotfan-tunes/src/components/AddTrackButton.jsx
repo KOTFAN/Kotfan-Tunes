1d907a2406a30890a238d412e5d28b8af449afb5
KOTFAN <kotfan.empire@gmail.com>
2025-07-20 18:57:53 +0300

    add AddTrackButton component

---
+++ frontend/Kotfan-tunes/src/components/AddTrackButton.jsx
@@ -0,0 +1,14 @@
+export const AddTrackButton = () => {
+  return (
+    <div className="addTrack">
+      <button
+        type="button"
+        className="text-text text-base pt-2 pb-2 pr-4 pl-4 bg-secondary hover:bg-bgHover hover:text-textHover transition-colors rounded-sm"
+      >
+        Add track
+      </button>
+    </div>
+  );
+};
+
+export default AddTrackButton;
--- frontend/Kotfan-tunes/src/pages/Tracks.jsx
+++ frontend/Kotfan-tunes/src/pages/Tracks.jsx
@@ -1,15 +1,9 @@
+import AddTrackButton from "../components/AddTrackButton";
 const Tracks = () => {
   return (
     <div>
       <div>Tracks page</div>
-      <div className="addTrack">
-        <button
-          type="button"
-          className="text-text text-base pt-2 pb-2 pr-4 pl-4 bg-secondary hover:bg-bgHover hover:text-textHover transition-colors rounded-sm"
-        >
-          Add track
-        </button>
-      </div>
+      <AddTrackButton />
     </div>
   );
 };
