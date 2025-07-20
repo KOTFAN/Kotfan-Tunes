c3ea2eaac3aaccd9091e97dd0da0cd639fbac12d
KOTFAN <kotfan.empire@gmail.com>
2025-07-20 18:39:51 +0300

    add Logo component

---
+++ frontend/Kotfan-tunes/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import logo from "../assets/logo.png";
+const Logo = () => {
+  return (
+    <div className="cursor-pointer">
+      <img src={logo} alt="Logo" className="h-10 w-auto" />
+    </div>
+  );
+};
+export default Logo;
--- frontend/Kotfan-tunes/src/components/Navigation.jsx
+++ frontend/Kotfan-tunes/src/components/Navigation.jsx
@@ -1,10 +1,10 @@
+import Logo from "./Logo";
 import Search from "./Search";
-import logo from "../assets/logo.png";
 
 const Navigation = () => {
   return (
     <div className="flex items-center justify-between px-6 py-4 w-full">
-      <img src={logo} alt="Logo" className="h-10 w-auto" />
+      <Logo />
 
       <Search />
     </div>
